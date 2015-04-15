8a760996928576e7df6a08c0164f46bc9976a65f
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-04-14 19:01:46 -0700

    Setting up Spyke, PStore, Dotenv

--- lib/cpw.rb
+++ lib/cpw.rb
@@ -1,5 +1,28 @@
-require "cpw/version"
+require "faraday"
+require "spyke"
+require "multi_json"
 
+require "cpw/version"
+require "cpw/json_parser"
+require "cpw/ingest"
+
+require "pstore"
+
+require "dotenv"
+Dotenv.load
+
+# Run: bundle exec irb -r "cpw"
 module CPW
-  Url = "http://voyz.es/posts/ruby/1"
+  Store = PStore.new("cpw.pstore")
+
+  Store.transaction do
+    Store[:access_token] = "abcd1234"
+  end
+  puts ENV['CLIENT_KEY']
+
+  Spyke::Base.connection = Faraday.new(url: "http://localhost:3000/api/") do |c|
+   c.request :json
+   c.use CPW::JsonParser
+   c.adapter Faraday.default_adapter
+  end
 end
