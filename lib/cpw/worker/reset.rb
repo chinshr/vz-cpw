a329cb6b9368e30a2347d1d038897a54f13c94ac
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-06-11 18:14:55 -0700

    Adding reset and stop workers

---
+++ lib/cpw/worker/reset.rb
@@ -0,0 +1,15 @@
+module CPW
+  module Worker
+    class Reset < Worker::Base
+      extend Worker::Helper
+
+      shoryuken_options queue: -> { queue_name },
+        auto_delete: true, body_parser: :json
+
+      def perform(sqs_message, body)
+        logger.info("+++ #{self.class.name}#perform, body #{body.inspect}")
+      end
+
+    end
+  end
+end
