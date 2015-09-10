b410a1cd3617d7e9063e18c5f4af7e78a9195aee
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-09-09 17:54:58 -0700

    Using different audio for file sample

--- samples/file.rb
+++ samples/file.rb
@@ -2,6 +2,6 @@ require 'pocketsphinx-ruby'
 
 recognizer = Pocketsphinx::AudioFileSpeechRecognizer.new
 
-recognizer.recognize('examples/assets/audio/goforward.raw') do |speech|
+recognizer.recognize('examples/assets/audio/hello.wav') do |speech|
   puts speech # => "go forward ten meters"
 end
