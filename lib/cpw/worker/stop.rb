e8d83420157b9ee6ffa6c86507103bcdf7fc411c
Juergen Fesslmeier <jfesslmeier@gmail.com>
2015-10-19 22:55:48 -0700

    Terminate after stopped

--- lib/cpw/worker/stop.rb
+++ lib/cpw/worker/stop.rb
@@ -9,9 +9,9 @@ module CPW
       def perform(sqs_message, body)
         logger.info("+++ #{self.class.name}#perform, body #{body.inspect}")
 
-        update_ingest({status: Ingest::STATE_STOPPED})
+        update_ingest({status: Ingest::STATE_STOPPED, terminate: true})
 
-        sqs_message.delete
+        terminate!
       end
 
     end
